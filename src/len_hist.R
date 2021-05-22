5b55ae9c16faa0ec7f53bfc82bec9f0d047e354d
YaroslavSolo <yaroslav.solo@yandex.ru>
2021-05-22 21:51:56 +0300

    scripts added

---
+++ src/len_hist.R
@@ -0,0 +1,21 @@
+source('lib.R')
+
+###
+
+#NAME <- 'H3K9me3_H9.ENCFF073SPO.hg19'
+#NAME <- 'H3K9me3_H9.ENCFF073SPO.hg38'
+#NAME <- 'H3K9me3_H9.ENCFF305RWK.hg19'
+NAME <- 'H3K9me3_H9.ENCFF305RWK.hg38'
+
+###
+
+bed_df <- read.delim(paste0(DATA_DIR, NAME, '.bed'), as.is = TRUE, header = FALSE)
+colnames(bed_df) <- c('chrom', 'start', 'end', 'name', 'score')
+bed_df$len <- bed_df$end - bed_df$start
+
+ggplot(bed_df) +
+  aes(x = len) +
+  geom_histogram() +
+  ggtitle(NAME, subtitle = sprintf('Number of peaks = %s', nrow(bed_df))) +
+  theme_bw()
+ggsave(paste0('len_hist.', NAME, '.pdf'), path = OUT_DIR)
