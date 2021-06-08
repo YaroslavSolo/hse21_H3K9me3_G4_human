4c75bb1541a86b27c959436e378ffb62fac9dce1
YaroslavSolo <yaroslav.solo@yandex.ru>
2021-06-08 19:11:11 +0300

    GO analisys

--- src/len_hist.R
+++ src/len_hist.R
@@ -2,17 +2,17 @@ source('lib.R')
 
 ###
 
-#NAME <- 'H3K9me3_H9.ENCFF073SPO.hg19'
-#NAME <- 'H3K9me3_H9.ENCFF073SPO.hg38'
-#NAME <- 'H3K9me3_H9.ENCFF305RWK.hg19'
-#NAME <- 'H3K9me3_H9.ENCFF305RWK.hg38'
-NAME <- 'GSE99205_common_HaCaT_G4_ChIP_peaks_RNase_treated'
+NAME <- 'H3K9me3_H9.ENCFF073SPO.hg19'
+NAME <- 'H3K9me3_H9.ENCFF073SPO.hg38'
+NAME <- 'H3K9me3_H9.ENCFF305RWK.hg19'
+NAME <- 'H3K9me3_H9.ENCFF305RWK.hg38'
+#NAME <- 'GSE99205_common_HaCaT_G4_ChIP_peaks_RNase_treated'
 
 ###
 
 bed_df <- read.delim(paste0(DATA_DIR, NAME, '.bed'), as.is = TRUE, header = FALSE)
-#colnames(bed_df) <- c('chrom', 'start', 'end', 'name', 'score')
-colnames(bed_df) <- c('chrom', 'start', 'end')
+colnames(bed_df) <- c('chrom', 'start', 'end', 'name', 'score')
+#colnames(bed_df) <- c('chrom', 'start', 'end')
 bed_df$len <- bed_df$end - bed_df$start
 
 ggplot(bed_df) +
@@ -20,4 +20,4 @@ ggplot(bed_df) +
   geom_histogram() +
   ggtitle(NAME, subtitle = sprintf('Number of peaks = %s', nrow(bed_df))) +
   theme_bw()
-ggsave(paste0('len_hist.', NAME, '.pdf'), path = OUT_DIR)
+ggsave(paste0('len_hist.', NAME, '.png'), path = OUT_DIR)
